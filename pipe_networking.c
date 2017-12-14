08250f6ec66fbc29ad7e335618e78ff52de8626f
Peter Lee <dlee14@stuy.edu>
2017-12-14 15:05:13 -0500

    updating handshake. kind of works

--- pipe_networking.c
+++ pipe_networking.c
@@ -2,24 +2,51 @@
 
 
 /*=========================
-  server_handshake
-  args: int * to_client
-  Perofrms the client side pipe 3 way handshake.
-  Sets *to_client to the file descriptor to the downstream pipe.
-  returns the file descriptor for the upstream pipe.
-  =========================*/
+server_handshake
+args: int * to_client
+Performs the client side pipe 3 way handshake.
+Sets *to_client to the file descriptor to the downstream pipe.
+returns the file descriptor for the upstream pipe.
+=========================*/
 int server_handshake(int *to_client) {
-  return 0;
+  int fd;
+  char * myfifo = "WKP";
+  int pid;
+  // Making the named pipe
+  if (mkfifo(myfifo, 0666)) {
+    printf("Error: FIFO already exists\n");
+  }
+  fd = open(myfifo, O_WRONLY);
+  printf("Server is reading...");
+  read(fd, &pid, sizeof(pid));
+  printf("pid read: %d", pid);
+  *to_client = fd;
+  close(fd);
+
+  return *to_client;
 }
 
 
 /*=========================
-  client_handshake
-  args: int * to_server
-  Perofrms the client side pipe 3 way handshake.
-  Sets *to_server to the file descriptor for the upstream pipe.
-  returns the file descriptor for the downstream pipe.
-  =========================*/
+client_handshake
+args: int * to_server
+Performs the client side pipe 3 way handshake.
+Sets *to_server to the file descriptor for the upstream pipe.
+returns the file descriptor for the downstream pipe.
+=========================*/
 int client_handshake(int *to_server) {
-  return 0;
+  int fd;
+  char * myfifo = "private";
+
+  // Making the named pipe
+  if (mkfifo(myfifo, 0666)) {
+    printf("Error: FIFO already exists\n");
+  }
+  fd = open(myfifo, O_WRONLY);
+  *to_server = fd;
+  int pid = getpid();
+  write(fd, &pid, sizeof(pid));
+  close(fd);
+
+  return *to_server;
 }
